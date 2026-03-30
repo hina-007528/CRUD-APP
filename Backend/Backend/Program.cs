3ab76ebc5a4d3c4f9e9cc12ff33cad2aa19c05bb
Shiraz@jylixtech.com <shiraz@jylixtech.com>
2026-03-31 02:20:06 +0500

    Swapped SQL Server for Neon PostgreSQL

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddSwaggerGen();
 
 string connectionString = builder.Configuration.GetConnectionString("Default") ?? throw new ArgumentNullException("connectionString is null");
 
-builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer(connectionString));
+builder.Services.AddDbContext<AppDbContext>(op => op.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
